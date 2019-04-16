a639f0efefc54508598b4c2b271c54a2999c074b
Julius Metz <julius.metz@tu-dresden.de>
2019-04-16 15:22:34 +0200

    take non-default interval from config into account

--- metricq_source_snmp/main.py
+++ metricq_source_snmp/main.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python
-
+from functools import reduce
 import asyncio
 import time
 import sys
@@ -65,20 +65,84 @@ async def getone(snmp_engine, host, community_string, objects):
         return ret
 
 
+def gcd(a, b):
+    """Return greatest common divisor using Euclid's Algorithm."""
+    while b:
+        a, b = b, a % b
+    return a
+
+
+def lcm(a, b):
+    """Return lowest common multiple."""
+    return a * b // gcd(a, b)
+
+
+def lcmm(*args):
+    """Return lcm of args."""
+    return reduce(lcm, args)
+
+
+def create_time_step_list(time_list):
+    """make a list with the sleep length"""
+    round_lcm = lcmm(*time_list)  # get least common multiple
+    one_round = set()  # only unique times
+
+    for time_step in time_list:
+        one_round.update(range(0, round_lcm, time_step))  # add the times where it is called
+
+    one_round = sorted(list(one_round))
+    step_list = []
+    last_value = 0
+    for elem in one_round:
+        step_list.append(elem - last_value)  # make List with times to step List
+        last_value = elem
+    return step_list
+
+
 async def collect_periodically(work, result_queue):
     snmp_engine = SnmpEngine()
+    time_list = []
+    for _, _, objects in work:
+        for _, _, interval in objects.values():
+            time_list.append(interval)
+
+    time_list = list(set(time_list))  # only unique intervals
+    time_step_list = create_time_step_list(time_list)
+
+    current_value = 0
+    position_step_list = 0
+    list_len = len(time_step_list)
+
     while True:
         get_data = []
         for host, community_string, objects in work:
-            get_data.append(getone(snmp_engine, (host, 161),
-                                   community_string, objects))
+            needed_objects = {}
+            for obj_id, obj_data in objects.items():
+                _, _, interval = obj_data
+                if current_value % interval == 0:
+                    needed_objects[obj_id] = obj_data
 
+            if needed_objects:
+                get_data.append(getone(snmp_engine, (host, 161),
+                                       community_string, needed_objects))
+
+        before_ts = time.time()
         ret = await asyncio.gather(*get_data)
+        req_duration = time.time() - before_ts
+
         #print("Worker putting {} results into queue...".format(len(ret)))
         for r in ret:
             if r:
                 result_queue.put_nowait(r)
-        await asyncio.sleep(INTERVAL)
+
+        if position_step_list == list_len:
+            position_step_list = 0
+            current_value = 0
+
+        current_value += time_step_list[position_step_list]
+        sleep_val = max(0, time_step_list[position_step_list] - req_duration)
+        await asyncio.sleep(sleep_val)
+        position_step_list += 1
 
 
 def do_work(input_queue, result_queue):
@@ -138,7 +202,7 @@ class PduSource(metricq.IntervalSource):
 
                 objects_by_host[host][obj_id] = (metric_name, multi, interval)
 
-        num_procs = config.get('num_procs', mp.cpu_count())
+        num_procs = min(config.get('num_procs', mp.cpu_count()), len(objects_by_host))
 
         chunked = chunks(list(objects_by_host.keys()), num_procs)
         work = mp.Queue()
