9622eb198f4230b77d3b190e0d7cf0547af0dc9f
Julius Metz <julius.metz@tu-dresden.de>
2019-05-13 16:28:49 +0200

    print changed

--- metricq_source_snmp/main.py
+++ metricq_source_snmp/main.py
@@ -220,7 +220,7 @@ def run(server, token):
         with aiomonitor.start_monitor(src.event_loop, locals={'src': src}):
             src.run()  # catch_signals=())
     except KeyboardInterrupt:
-        print('Keyboard interrupt Exit Process')
+        print('Keyboard interrupt, exiting Process')
 
 
 if __name__ == "__main__":
