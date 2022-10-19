9556acc5dd9155146957bce1d12b838fc145e217
Mario Bielert <mario.bielert@tu-dresden.de>
2022-10-19 10:55:19 +0200

    reformat with black and isort

--- metricq_source_snmp/main.py
+++ metricq_source_snmp/main.py
@@ -1,29 +1,39 @@
 #!/usr/bin/python
-from functools import reduce
 import asyncio
-import time
-import sys
-from collections import defaultdict
-import multiprocessing as mp
-from queue import Empty
 import logging
 import logging.handlers
+import multiprocessing as mp
 import signal
+import sys
+import time
+from collections import defaultdict
+from functools import reduce
+from queue import Empty
+
+import aiomonitor
 import click
 import click_log
-import aiomonitor
 import metricq
-from pysnmp.hlapi.asyncio import getCmd, ObjectType, ObjectIdentity, CommunityData, ContextData, SnmpEngine, UdpTransportTarget
 from metricq.logging import get_logger
+from pysnmp.hlapi.asyncio import (
+    CommunityData,
+    ContextData,
+    ObjectIdentity,
+    ObjectType,
+    SnmpEngine,
+    UdpTransportTarget,
+    getCmd,
+)
 
-NaN = float('nan')
+NaN = float("nan")
 
 logger = get_logger()
 click_log.basic_config(logger)
 # Use this if we ever use threads
 # logger.handlers[0].formatter = logging.Formatter(fmt='%(asctime)s %(threadName)-16s %(levelname)-8s %(message)s')
 logger.handlers[0].formatter = logging.Formatter(
-    fmt='%(asctime)s [%(levelname)-8s] [%(name)-20s] %(message)s')
+    fmt="%(asctime)s [%(levelname)-8s] [%(name)-20s] %(message)s"
+)
 orig_sig_handler = {}
 
 
@@ -38,7 +48,7 @@ async def get_one(snmp_engine, host, community_string, objects):
             ContextData(),
             *objs,
         )
-       
+
         ts = metricq.Timestamp.now()
 
         if errorIndication:
@@ -46,15 +56,15 @@ async def get_one(snmp_engine, host, community_string, objects):
 
         if errorStatus:
             error_msg = errorStatus.prettyPrint()
-            raise RuntimeError(f'{error_msg} at {errorIndex}')
+            raise RuntimeError(f"{error_msg} at {errorIndex}")
 
     except Exception as e:
         # ts might not be defined here, if getCmd had raised an exception
-        ts = metricq.Timestamp.now() 
+        ts = metricq.Timestamp.now()
         logging.error(host, e)
         return [(metric_name, ts, NaN) for metric_name, _, _ in objects.values()]
 
-    assert(len(varBinds) == len(objects))
+    assert len(varBinds) == len(objects)
 
     data_points = []
 
@@ -75,14 +85,15 @@ async def collect_periodically(work, result_queue, interval):
     while True:
         get_data = []
         while deadline <= time.time():
-            logging.warning('missed deadline')
+            logging.warning("missed deadline")
             deadline += interval
         sleep_var = deadline - time.time()
         await asyncio.sleep(sleep_var)
         deadline += interval
         for host, community_string, objects in work:
-            get_data.append(get_one(snmp_engine, (host, 161),
-                                    community_string, objects))
+            get_data.append(
+                get_one(snmp_engine, (host, 161), community_string, objects)
+            )
         ret = await asyncio.gather(*get_data)
         for r in ret:
             if r:
@@ -99,12 +110,10 @@ async def do_work(input_queue, result_queue):
             sorted_objects[interval][obj_id] = obj_data
 
         for object_interval, object_data in sorted_objects.items():
-            sorted_work[object_interval].append(
-                (host, community_string, object_data))
+            sorted_work[object_interval].append((host, community_string, object_data))
     work_loops = []
     for work_interval, work_data in sorted_work.items():
-        work_loops.append(collect_periodically(
-            work_data, result_queue, work_interval))
+        work_loops.append(collect_periodically(work_data, result_queue, work_interval))
     await asyncio.gather(*work_loops)
 
 
@@ -128,9 +137,17 @@ class SnmpSource(metricq.IntervalSource):
         self.workers = None
         self.result_queue = mp.Queue()
 
-    @metricq.rpc_handler('config')
-    async def _on_config(self, default_community, default_interval, default_prefix, default_object_collections,
-                         additional_metric_attributes, snmp_object_collections, **config):
+    @metricq.rpc_handler("config")
+    async def _on_config(
+        self,
+        default_community,
+        default_interval,
+        default_prefix,
+        default_object_collections,
+        additional_metric_attributes,
+        snmp_object_collections,
+        **config,
+    ):
         self.period = metricq.Timedelta.from_s(1)
         metrics = {}  # holds metrics for declaration to metricq
 
@@ -138,28 +155,26 @@ class SnmpSource(metricq.IntervalSource):
         objects_by_host = defaultdict(dict)
         community_by_host = {}
 
-        for host, host_cfg in config['hosts'].items():
-            community_by_host[host] = host_cfg.get(
-                'community', default_community)
+        for host, host_cfg in config["hosts"].items():
+            community_by_host[host] = host_cfg.get("community", default_community)
 
-            host_interval = host_cfg.get('interval', default_interval)
+            host_interval = host_cfg.get("interval", default_interval)
 
             objs = {}
-            obj_col_list = host_cfg.get('objects', default_object_collections)
+            obj_col_list = host_cfg.get("objects", default_object_collections)
             for obj_col in obj_col_list:
                 objs.update(snmp_object_collections[obj_col])
 
             for obj_id, obj in objs.items():
                 description = "{} {}".format(
-                    host_cfg['description'], obj['short_description'])
+                    host_cfg["description"], obj["short_description"]
+                )
                 metric_name = "{}.{}.{}".format(
-                    default_prefix, host_cfg['infix'], obj['suffix'])
-                interval = obj.get('interval', host_interval)
+                    default_prefix, host_cfg["infix"], obj["suffix"]
+                )
+                interval = obj.get("interval", host_interval)
 
-                metric = {
-                    'rate': 1.0 / interval,
-                    'description': description
-                }
+                metric = {"rate": 1.0 / interval, "description": description}
 
                 for metric_attr in additional_metric_attributes:
                     if metric_attr in obj:
@@ -168,18 +183,21 @@ class SnmpSource(metricq.IntervalSource):
                         metric[metric_attr] = host_cfg[metric_attr]
                 metrics[metric_name] = metric
 
-                multi = obj.get('multiplier', 1.0)
+                multi = obj.get("multiplier", 1.0)
 
                 objects_by_host[host][obj_id] = (metric_name, multi, interval)
 
-        num_procs = min(config.get('num_procs', mp.cpu_count()),
-                        len(objects_by_host))
+        num_procs = min(config.get("num_procs", mp.cpu_count()), len(objects_by_host))
 
         chunked = chunks(list(objects_by_host.keys()), num_procs)
         work = mp.Queue()
         for part in chunked:
-            work.put([(host, community_by_host[host], objects_by_host[host])
-                      for host in part])
+            work.put(
+                [
+                    (host, community_by_host[host], objects_by_host[host])
+                    for host in part
+                ]
+            )
 
         logger.info("Starting {} worker processes...".format(num_procs))
         sys.stdout.flush()
@@ -189,9 +207,11 @@ class SnmpSource(metricq.IntervalSource):
             self.workers.terminate()
 
         original_sigint_handler = signal.signal(
-            signal.SIGINT, orig_sig_handler['interrupt'])
+            signal.SIGINT, orig_sig_handler["interrupt"]
+        )
         original_sigterm_handler = signal.signal(
-            signal.SIGTERM, orig_sig_handler['terminate'])
+            signal.SIGTERM, orig_sig_handler["terminate"]
+        )
 
         self.workers = mp.Pool(num_procs, mp_worker, (work, self.result_queue))
         signal.signal(signal.SIGINT, original_sigint_handler)
@@ -206,7 +226,7 @@ class SnmpSource(metricq.IntervalSource):
             try:
                 result_list = self.result_queue.get_nowait()
                 for metric_name, ts, value in result_list:
-                    #print(metric_name, ts, value)
+                    # print(metric_name, ts, value)
                     self[metric_name].append(ts, value)
                     send_metric_count += 1
             except Empty:
@@ -216,24 +236,27 @@ class SnmpSource(metricq.IntervalSource):
             await self.flush()
         except Exception as e:
             logger.error("Exception in send: {}".format(str(e)))
-        logger.info("Send took {:.2f} seconds, count: {}".format(
-            time.time() - ts_before, send_metric_count))
+        logger.info(
+            "Send took {:.2f} seconds, count: {}".format(
+                time.time() - ts_before, send_metric_count
+            )
+        )
 
 
 @click.command()
-@click.option('--server', default='amqp://localhost/')
-@click.option('--token', default='source-py-snmp')
+@click.option("--server", default="amqp://localhost/")
+@click.option("--token", default="source-py-snmp")
 @click_log.simple_verbosity_option(logger)
-def run(server, token):
-    orig_sig_handler['interrupt'] = signal.getsignal(signal.SIGINT)
-    orig_sig_handler['terminate'] = signal.getsignal(signal.SIGTERM)
+def run(server, token) -> None:
+    orig_sig_handler["interrupt"] = signal.getsignal(signal.SIGINT)
+    orig_sig_handler["terminate"] = signal.getsignal(signal.SIGTERM)
     try:
         src = SnmpSource(token=token, management_url=server)
         loop = asyncio.get_event_loop()
-        with aiomonitor.start_monitor(loop=loop, locals={'src': src}):
+        with aiomonitor.start_monitor(loop=loop, locals={"src": src}):
             src.run()
     except KeyboardInterrupt:
-        print('Keyboard interrupt, exiting process')
+        print("Keyboard interrupt, exiting process")
 
 
 if __name__ == "__main__":
